d3cce9d56f0f64076ea1dead32f1411404361aed
DemetriceW20 <demetrice_williams@hotmail.com>
2022-12-11 09:34:58 -0700

    install firebase tools. add firebase config. removed test files.

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiQ9lR0K2YE-WTitXnZaSktNIw_aSIe9M",
+  authDomain: "netflix-clone-fd696.firebaseapp.com",
+  projectId: "netflix-clone-fd696",
+  storageBucket: "netflix-clone-fd696.appspot.com",
+  messagingSenderId: "356728070999",
+  appId: "1:356728070999:web:22f5c9557410bedcc19412"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+// THIS IS FIREBASE CONFIG FILE
