e1efaec893c040254d1b06fe5310ddf5e951aac8
DemetriceW20 <demetrice_williams@hotmail.com>
2022-12-11 10:02:08 -0700

    removed counter files. add home screen.

---
+++ netflix-clone/src/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./HomeScreen.css";
+
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+        {/* NAVBAR */}
+
+        {/* BANNER */}
+
+
+        {/* ROW */}
+
+    </div>
+  )
+}
+
+export default HomeScreen
